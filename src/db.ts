caa73cdaf52c902d5d7acf1ebab99dd05b835003
Colin Tindle <ampersand@underscore.space>
2022-02-14 23:30:27 +0000

    add prisma and sample schema

---
+++ src/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+process.on("beforeExit", () => {
+    prisma.$disconnect();
+});
+
+export default prisma;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello ts");
+import db from "./db";
+
+(async () => {
+    // await db.permissionType.create({ data: { name: "new type" } });
+    const permissionTypes = await db.permissionType.findMany();
+    console.dir({ permissionTypes });
+})();
